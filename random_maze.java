f1c4fbe939c6e0725ac31caa85b5b4e12046c29d
Teen Ho <konstantin.bryu@mail.ru>
2022-03-14 00:40:04 +0300

    Add class 'Maze' with constructor, getters and setters for the length and width of the maze.

--- random_maze.java
+++ random_maze.java
@@ -1,26 +1,25 @@
 package my_lessons;
 
+import java.util.Scanner;
+
 public class random_maze {
 
 	public static void main(String[] args) {
-		//длина лабиринта
-		int a = 30;
-		//ширина лабиринта
-		int b = 30;
-		String[][] maze = new String[a][b];
-		String[] element1 = new String[] {"+", "-", "|", " "};
+		
+		Maze MyMaze = new Maze(30, 15);
+		
+		String[][] maze = new String[MyMaze.getLength()][MyMaze.getWidth()];
 		String[] element2 = new String[] {"+", "-"};
 		String[] element3 = new String[] {"-", " "};
 		String[] element4 = new String[] {"|", "+"};
 		String[] element5 = new String[] {"|", " "};
 		String[] element6 = new String[] {"+", " "};
-		
+	
 		//формирование начального элемента лабиринта
-		maze[0][0] = element1[(int)(Math.random() * 10) / 3];
-		System.out.print(maze[0][0]);
+		maze[0][0] = MazeStartElement();
 		
 		//заполнение горизонтальной границы лабиринта
-		for (int i = 1; i < a; i++) {
+		for (int i = 1; i < MyMaze.getLength(); i++) {
 			if (maze[i - 1][0] == " ")
 				maze[i][0] = "+";
 			else if (maze[i - 1][0] == "|")
@@ -35,7 +34,7 @@ public class random_maze {
 		System.out.println();
 		
 		//заполнение вертикальной границы лабиринта
-		for (int j = 1; j < b; j++) {
+		for (int j = 1; j < MyMaze.getWidth(); j++) {
 			if (maze[0][j - 1] == " ")
 				maze[0][j] = element2[(int)(Math.random() * 10) / 5];
 			else if (maze[0][j - 1] == "|")
@@ -47,8 +46,8 @@ public class random_maze {
 		}
 		
 		//заполнение оставшейся зоны лабиринта
-		for (int j = 1; j < b; j++) {
-			for (int i = 0; i < a; i++) {
+		for (int j = 1; j < MyMaze.getWidth(); j++) {
+			for (int i = 0; i < MyMaze.getLength(); i++) {
 				if (i > 0) {
 					if (maze[i][j - 1] == " ") {
 						if (maze[i - 1][j] == " ") {
@@ -90,5 +89,45 @@ public class random_maze {
 		}
 
 	}
+	
+	public static String MazeStartElement() {
+		String[] element1 = new String[] {"+", "-", "|", " "};
+		String maze = element1[(int)(Math.random() * 10) / 3];
+		System.out.print(maze);
+		return maze;
+	}
+	
+	//public int getMazeLength() {
+	//	Scanner sc = new Scanner(System.in);
+	//	int MazeLength = sc.nextInt();
+	//	return MazeLength;
+	//}
 
+}
+
+class Maze {
+	private int Length;
+	private int Width;
+	
+	public Maze (int Length, int Width) {
+		this.Length = Length;
+		this.Width = Width;
+	}
+	
+	public int getLength() {
+		return Length;
+	}
+	
+	public int getWidth() {
+		return Width;
+	}
+	
+	public void setLength(int Length) {
+		this.Length = Length;
+	}
+	
+	public void setWidth(int Width) {
+		this.Width = Width;
+	}
+	
 }
