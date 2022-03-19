bba972f13e1096a0cc23478ac51a46ea6eacea46
Teen Ho <konstantin.bryu@mail.ru>
2022-03-19 16:17:11 +0300

    Added a new code for the randomly generated maze "newrandom_maze".

---
+++ newrandom_maze.java
@@ -0,0 +1,100 @@
+package my_lessons;
+
+public class random_maze {
+
+	public static void main(String[] args) {
+		new Maze(29, 15);
+		
+	}
+}
+
+class Maze {
+	private int Length;
+	private int Width;
+	
+	public Maze (int Length, int Width) {
+		this.Length = Length;
+		this.Width = Width;
+		
+		String[][] maze = new String[this.Length][this.Width];
+		
+		String symbol1 = " ";
+		
+		for (int j = 0; j < this.Width; j += 2) {
+			for (int i = 0; i < this.Length; i += 2) {
+				maze[i][j] = "+";
+			}
+		}
+		
+		for (int j = 1; j < this.Width; j += 2) {
+			for (int i = 1; i < this.Length; i += 2) {
+				maze[i][j] = " ";
+			}
+		}
+		
+		//заполнение верхней границы
+		for (int i = 1; i < this.Length; i += 2) {
+			maze[i][0] = RandomElement("-", symbol1);
+			if (maze[i][0] == " ")
+				symbol1 = "-";
+		}
+		
+		symbol1 = " ";
+		
+		//заполнение нижней границы
+		for (int i = 1; i < this.Length; i += 2) {
+			maze[i][this.Width - 1] = RandomElement("-", symbol1);
+			if (maze[i][this.Width - 1] == " ")
+				symbol1 = "-";
+		}
+		
+		//заполнение боковых границ
+		for (int j = 1; j < this.Width; j += 2) {
+			maze[0][j] = "|";
+			maze[this.Length - 1][j] = "|";
+		}
+		
+		//заполнение внутренней части лабиринта
+		for (int j = 1; j < this.Width; j += 2) {
+			for (int i = 2; i < this.Length - 1; i += 2) {
+				maze[i][j] = RandomElement("|", " ");
+			}
+		}
+		
+		for (int j = 2; j < this.Width - 1; j += 2) {
+			for (int i = 1; i < this.Length; i += 2) {
+				maze[i][j] = RandomElement("-", " ");
+			}
+		}
+		
+		//вывод лабиринта
+		for (int j = 0; j < this.Width; j++) {
+			for (int i = 0; i < this.Length; i++) {
+				System.out.print(maze[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public int getLength() {
+		return Length;
+	}
+	
+	public int getWidth() {
+		return Width;
+	}
+	
+	public void setLength(int Length) {
+		this.Length = Length;
+	}
+	
+	public void setWidth(int Width) {
+		this.Width = Width;
+	}
+	
+	public String RandomElement(String element1, String element2) {
+		String[] elements = new String[] {element1, element2};
+		return elements[(int)(Math.random() * 10) / 5];
+	}
+}
